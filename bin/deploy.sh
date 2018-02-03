d308b1b4bf0a1d252e94b700a9cec5c02f1a6ca6
Felix Seidel <felix@seidel.me>
2018-02-03 17:59:33 +0100

    Site updated at 2018-02-03T17:59:33+01:00

---
+++ bin/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+main() {
+  if ! [[ -d "$DIR/../_site/.git" ]]; then
+    >&2 echo "Please clone the master branch to _site!"
+    exit 1
+  fi
+
+  #docker-compose build app
+  docker-compose up -d app
+  docker-compose exec app bin/build-resume.js
+  docker-compose run prod grunt prod
+  cd "$DIR/../_site"
+  git add -A
+  git commit --no-verify -m "Site updated at $(date -Iseconds)"
+  git push
+}
+
+main "$@"
